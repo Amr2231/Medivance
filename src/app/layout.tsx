abb4598be5cd9030f6678dd57b7d386242978dd6
Amr2231 <amr540290@gmail.com>
2026-08-12 16:16:51 +0300

    fix(csp): read headers() in root layout to apply nonce to inline scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,17 @@
 import "./globals.css";
 import Provider from "@/components/providers/shared";
+import { headers } from "next/headers";
+
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  await headers();
+
   return (
-    <html
-      lang="en"
-      suppressHydrationWarning
-      className="font-sans"
-    >
-      <body
-        className="antialiased"
-      >
+    <html lang="en" suppressHydrationWarning className="font-sans">
+      <body className="antialiased">
         <Provider>{children}</Provider>
       </body>
     </html>
